ee53be4f3fcd010354841f886842b6063c9e7b0d
Jesús Fernando Carrasco Mendoza <jefecame@hotmail.com>
2024-04-14 00:11:19 -0700

    configuracion

--- decisiones_ciclos/laptops/laptops.cpp
+++ decisiones_ciclos/laptops/laptops.cpp
@@ -1,7 +1,13 @@
+
 #include <iostream>
 
 using namespace std;
 
+/**
+ * @brief   This is the main function of the program.
+ *          It prints "Hello, World!" to the console.
+ * @return  0 indicating successful execution of the program.
+ */
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
